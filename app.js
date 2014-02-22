c470494f704c3171457e2e1786999b23ee769e84
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-02-22 18:03:49 -0500

    Add Step 5b

--- app.js
+++ app.js
@@ -2,13 +2,27 @@
 
 angular.module('app', [])
   .controller('TaskListCtrl', function ($scope, $http) {
-    $http.get('api/tasks').success(function (data) {
-      $scope.tasks = data;
-    });
+    getTasks();
 
     $scope.refresh = function () {
-      $http.get('api/tasks').success(function (data) {
+      getTasks();
+    };
+
+    function getTasks () {
+      $http.get('tasks.json').success(function (data) {
         $scope.tasks = data;
+      });
+    }
+  })
+  .controller('NewTaskCtrl', function ($scope, $http) {
+    $scope.taskName = null;
+    $scope.createTask = function () {
+      var data = { name: $scope.taskName };
+      $http.post('api/tasks', data).success(function (data, status, headers) {
+        alert('Task added!');
+        $http.get(headers('location')).success(function (data) {
+          $scope.tasks.push(data);
+        });
       });
     };
   });
