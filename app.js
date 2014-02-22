a93b65cbe5d9e6780b33746f9c41bd2daea979f7
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-02-22 18:49:43 -0500

    Add Step 6a
    http://lostechies.com/gabrielschenker/2013/12/28/angularjspart-6-templates/

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 'use strict';
 
 angular.module('app', [])
-  .controller('TaskListCtrl', function ($scope, $http) {
-    getTasks();
-
-    $scope.refresh = function () {
-      getTasks();
-    };
-
-    function getTasks () {
-      $http.get('tasks.json').success(function (data) {
-        $scope.tasks = data;
-      });
-    }
-  })
-  .controller('NewTaskCtrl', function ($scope, $http) {
-    $scope.taskName = null;
-    $scope.createTask = function () {
-      var data = { name: $scope.taskName };
-      $http.post('api/tasks', data).success(function (data, status, headers) {
-        alert('Task added!');
-        $http.get(headers('location')).success(function (data) {
-          $scope.tasks.push(data);
-        });
-      });
-    };
+  .controller('PersonCtrl', function ($scope) {
+    $scope.persons = [{
+      name: { first: 'Henry', middle: 'Jacob', last: 'Mendocino' },
+      gender: 'Male'
+    }, {
+      name: { first: 'Ann', middle: 'Cecilia', last: 'Negro' },
+      gender: 'Female'
+    }, {
+      name: { first: 'Berta', middle: 'Ann', last: 'Sallyfield' },
+      gender: 'Female'
+    }, {
+      name: { first: 'Rudolf', middle: 'John', last: 'Waters' },
+      gender: 'Male'
+    }, {
+      name: { first: 'Ken', middle: 'Adam', last: 'Aundry' },
+      gender: 'Male'
+    }];
   });
