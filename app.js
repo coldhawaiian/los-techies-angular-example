bc7050c00f0f8be5176a7bf11ed1415369a6aec9
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-02-22 19:49:00 -0500

    Add Step 6b

--- app.js
+++ app.js
@@ -2,20 +2,26 @@
 
 angular.module('app', [])
   .controller('PersonCtrl', function ($scope) {
-    $scope.persons = [{
-      name: { first: 'Henry', middle: 'Jacob', last: 'Mendocino' },
-      gender: 'Male'
-    }, {
-      name: { first: 'Ann', middle: 'Cecilia', last: 'Negro' },
-      gender: 'Female'
-    }, {
-      name: { first: 'Berta', middle: 'Ann', last: 'Sallyfield' },
-      gender: 'Female'
-    }, {
-      name: { first: 'Rudolf', middle: 'John', last: 'Waters' },
-      gender: 'Male'
-    }, {
-      name: { first: 'Ken', middle: 'Adam', last: 'Aundry' },
-      gender: 'Male'
-    }];
+    $scope.persons = [
+      {
+        name: { first: 'Henry', middle: 'Jacob', last: 'Mendocino' },
+        gender: 'Male'
+      },
+      {
+        name: { first: 'Ann', middle: 'Cecilia', last: 'Negro' },
+        gender: 'Female'
+      },
+      {
+        name: { first: 'Berta', middle: 'Ann', last: 'Sallyfield' },
+        gender: 'Female'
+      },
+      {
+        name: { first: 'Rudolf', middle: 'John', last: 'Waters' },
+        gender: 'Male'
+      },
+      {
+        name: { first: 'Ken', middle: 'Adam', last: 'Aundry' },
+        gender: 'Male'
+      }
+    ];
   });
