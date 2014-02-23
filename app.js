caf2ad6bfe1e4b65f4428c3a434b4ff01d2238a3
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-02-22 21:59:20 -0500

    Add Step 6d

--- app.js
+++ app.js
@@ -33,4 +33,34 @@ angular.module('app', [])
       { number: '4', question: 'What is your favorite color?', type: 'singleOption' },
       { number: '5', question: 'Which of the following sweets do you like?', type: 'multiOption'}
     ];
+  })
+  .controller('TreeCtrl', function ($scope) {
+    $scope.tree = [
+      {
+        name: 'Parent 1',
+        children: [
+          {
+            name: 'Child 1.1',
+            children: [
+              { name: 'Grandchild 1.1.1' },
+              { name: 'Grandchild 1.1.2' },
+              { name: 'Grandchild 1.1.3' }
+            ]
+          },
+          { name: 'Child 1.2' },
+          { name: 'Child 1.3' }
+        ]
+      },
+      {
+        name: 'Parent 2',
+        children: [
+          { name: 'Child 2.1' },
+          { name: 'Child 2.2' },
+          { name: 'Child 2.3' }
+        ]
+      },
+      { name: 'Parent 3' },
+      { name: 'Parent 4' },
+      { name: 'Parent 5' }
+    ];
   });
