d08998992642ae04e990891494b097d4c811aa9e
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-02-22 21:48:05 -0500

    Add Step 6c

--- app.js
+++ app.js
@@ -24,4 +24,13 @@ angular.module('app', [])
         gender: 'Male'
       }
     ];
+  })
+  .controller('SurveyCtrl', function ($scope) {
+    $scope.questions = [
+      { number: '1', question: 'How old are you?', type: 'integer' },
+      { number: '2', question: 'Date of birth?', type: 'date' },
+      { number: '3', question: 'Describe yourself in a few words.', type: 'text' },
+      { number: '4', question: 'What is your favorite color?', type: 'singleOption' },
+      { number: '5', question: 'Which of the following sweets do you like?', type: 'multiOption'}
+    ];
   });
